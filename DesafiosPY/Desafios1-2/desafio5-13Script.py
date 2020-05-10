f79a0b79ca1d86b2f33757ee2759ed94e56875d0
Pedro Victor <paf.victor@gmail.com>
2020-05-10 14:15:10 -0300

    Resolução desafios 5-13

---
+++ DesafiosPY/Desafios1-2/desafio5-13Script.py
@@ -0,0 +1,47 @@
+#Desafio 5
+'''
+num = int(input())
+print('Sucessor: {}\nAntecessor: {}'.format(num+1,num-1))
+'''
+#Desafio 6
+'''
+x = float(input())
+print('Dobro: {}\nTriplo: {}\nRaiz quadrada: {}'.format(x*2,x*3,x**(1/2)))
+'''
+#Desafio 7
+'''
+nota1,nota2 = float(input()), float(input())
+print('Média = {}'.format((nota1+nota2)/2))
+'''
+#Desafio 8
+'''
+metros = float(input())
+print(f'Centimentros: {metros*100:.2f}\nMilimetros: {metros*1000:.2f}')
+'''
+#Desafio 9
+'''
+num = float(input())
+for x in [0,1,2,3,4,5,6,7,8,9,10]:
+    print(f'{num:.2f} * {x}= {num*x:.2f}\n')
+'''
+#Desafio 10
+'''
+reais = float(input())
+print(f'Voce pode comprar {reais/3.27} dólares')
+'''
+#Desafio 11
+'''
+largura = float(input())
+altura = float(input())
+print(f'A área dessa parede é {altura*largura:.2f}m² e é preciso {(altura*largura)/2:.2f}l de tinta')
+'''
+#Desafio 12
+'''
+preco = float(input())
+print(f'Novo preço: {preco-(preco*(5/100))}')
+'''
+#Desafio 13
+'''
+sal = float(input())
+print(f'Novo salário: {sal+(sal*(15/100))}')
+'''
