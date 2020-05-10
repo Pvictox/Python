563d1b9d805b192891dd030fb9715924c86c0d87
Pedro Victor <paf.victor@gmail.com>
2020-05-10 15:50:49 -0300

    Soma simples

---
+++ URI/somaSimples.py
@@ -0,0 +1,10 @@
+'''
+Leia dois valores inteiros, no caso para variáveis A e B. 
+A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+A seguir escrever o valor desta variável.
+'''
+
+a,b = int(input()), int(input())
+SOMA = a+b
+print(f'SOMA = {SOMA}')
+print('\n')
