36ef5c8b76de23180e3b3662aa0c9ea354ab3864
Pedro Victor <paf.victor@gmail.com>
2020-05-10 10:29:19 -0300

    Adicionado uma nova forma de exibição com .format

--- DesafiosPY/Desafios1-2/desafio3Script.py
+++ DesafiosPY/Desafios1-2/desafio3Script.py
@@ -1,9 +1,15 @@
-#sem pych
+#Primeira Forma
+'''
 num1 = input('Digite')
 num2 =input('Digite')
 #String to int
 int_num1 = int(num1)
 int_num2 = int(num2)
 print(int_num1+int_num2)
+'''
+#Segunda Forma
+a = int(input())
+b = int(input())
+print('A soma é {}'.format(a+b))
 
 
