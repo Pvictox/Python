05d61ff1d4903ee6778705b4d44e2efe5e1c6cee
Pedro Victor <paf.victor@gmail.com>
2020-05-10 15:32:05 -0300

    Resolução desafios 14-15

---
+++ DesafiosPY/Desafios1-2/desafio14-15Script.py
@@ -0,0 +1,8 @@
+#Desafio 14
+'''
+cel = float(input())
+print(f'{cel*(9/5)+32} farenheit')
+'''
+#Desafio 15
+quantKM, dias = float(input()), int(input())
+print(f'Valor a pagar: {(dias*60)+(0.15*quantKM):.2f}')
