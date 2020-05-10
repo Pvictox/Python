4f334758bae796affed9129c24e6893809e6a46e
Pedro Victor <paf.victor@gmail.com>
2020-05-10 15:51:08 -0300

    Produto simples

---
+++ URI/produtoSimples.py
@@ -0,0 +1,8 @@
+'''
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+A seguir mostre a variável PROD com mensagem correspondente.    
+'''
+a,b = int(input()), int(input())
+PROD = a*b
+print(f'PROD = {PROD}')
+print('\n')
