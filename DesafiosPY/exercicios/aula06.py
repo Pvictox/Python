de2691327d9cc45dd6fc1511ca5614e7ec211da1
Pedro Victor <paf.victor@gmail.com>
2020-05-10 10:55:46 -0300

    Conclusão dos exercícios aula 06

---
+++ DesafiosPY/exercicios/aula06.py
@@ -0,0 +1,11 @@
+#Exercício 01
+'''
+x = float(input())
+y = float(input())
+print('A soma de {} + {} é {}'.format(x,y,x+y))
+'''
+#Exercício 02
+'''
+x = input()
+print('Variavel do tipo {}\nSeu conteúdo é numérico? {}\nSeu conteúdo é composto só de letras? {}\nSeu conteúdo é alphaNum? {}'.format(type(x), x.isnumeric(), x.isalpha(), x.isalnum()))
+'''
