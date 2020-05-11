51dacd0867134cf72c46c517216429ddf6b3f166
Pedro Victor <paf.victor@gmail.com>
2020-05-11 13:16:55 -0300

    Resolução desafios 16-20

---
+++ DesafiosPY/Desafios1-2/desafio16-21Script.py
@@ -0,0 +1,35 @@
+#Desafio 16
+'''
+from math import floor
+n = float(input())
+print(f'{floor(n)}')
+'''
+#Desafio 17
+'''
+from math import sqrt,pow
+cat1, cat2 = float(input()), float(input())
+hip = pow(cat1,2)+pow(cat2,2)
+print(f'{sqrt(hip)}')
+'''
+#Desafio 18
+'''
+from math import sin, cos,tan
+ang = int(input())
+print(f'Seno: {sin(ang):.1f}\nCosseno: {cos(ang):.1f}\nTangente: {tan(ang):.1f}')
+'''
+#Desafio 19
+'''
+from random import choice,seed
+seed()
+n1,n2,n3,n4 = input(),input(),input(),input()
+print(f'O escolhido foi {choice([n1,n2,n3,n4])}')
+'''
+#Desafio 20
+'''
+from random import sample
+n1,n2,n3,n4 = input(),input(),input(),input()
+print(f'{sample([n1,n2,n3,n4],4)}')
+'''
+#Desafio 21
+
+
