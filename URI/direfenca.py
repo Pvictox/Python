c8492036ea61f145487907ddc7c19df9ae68a7c3
Pedro Victor <paf.victor@gmail.com>
2020-05-12 11:06:28 -0300

    Diferença

---
+++ URI/direfenca.py
@@ -0,0 +1,6 @@
+'''
+Leia quatro valores inteiros A, B, C e D. A seguir, 
+calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+'''
+a,b,c,d = int(input()),int(input()),int(input()),int(input())
+print(f'DIFERENCA = {(a*b)-(c*d)}\n')
