32b16ad0f50fa41b073f6251b388bc61e5103f28
Pedro Victor <paf.victor@gmail.com>
2020-05-15 15:59:07 -0300

    Consumo

---
+++ URI/consumo.py
@@ -0,0 +1,6 @@
+'''
+Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+'''
+dist = int(input())
+litros = float(input())
+print(f'{dist/litros:.3f} km/l\n')
